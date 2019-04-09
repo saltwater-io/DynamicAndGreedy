d1f7e0fc57a481e71286a9d2483968cf1bdec43f
Dakota McGuire <30912971+saltwater-io@users.noreply.github.com>
2019-04-09 14:01:04 -0500

    Update Project3.py

--- Project3.py
+++ Project3.py
@@ -1,9 +1,10 @@
-'''
-    This program implements a regular and dynamic solution to the rod cutting problem,
-    as well as an implementation of a greedy algorithm to solve a scheduling problem.
-    Written by: Dakota McGuire - 4/2/19
-    For Algorithms - USM
-'''
+
+#      This program implements a regular and dynamic solution to the rod cutting problem;
+#      as well as an implementation of a greedy algorithm to solve a scheduling problem.
+#
+#           Written by: Dakota McGuire - 4/2/19
+#               For Algorithms - USM
+
 # rod_prices = {1: 1, 2: 5, 3: 8, 4: 9, 5: 10, 6: 17, 7: 17, 8: 20, 9: 24, 10: 30}
 
 # Price of each length of section of rod
@@ -24,22 +25,29 @@ def get_max(a, b):
         return b
 
 
-# Cuts rod w/ repetition - not dynamic
+# Cuts rod recursively w/ repetition - not dynamic
 def cut_rod(price, n):
+    # If n = 0 or less return 0
     if not n > 0:
         return 0
-    max_value = -32767
+    max_value = 0
+    # Cuts rod into different pieces to find the best choice for maximum profit
     for i in range(0, n):
         max_value = get_max(max_value, price[i] + cut_rod(price, n - i - 1))
     return max_value
 
 
-# Cuts rod dynamically from bottom-up
+# Cuts rod dynamically by building an array of values bottom-up
+# Returns the maximum value sotred at index n in stored_prices
 def cut_bottom_up(price, n):
+    # Stored prices hold the max possible value of a rod of a certain length
     stored_prices = []
+    # From to 0 the the len(n) store '0' to hold the index
     for i in range(0, n + 1):
         stored_prices.append(0)
     max_value = 0
+    # Inserts maximum value of rod cuts into stored prices -
+    # using this method it checks previously stored values and compares
     for j in range(0, n + 1):
         for k in range(j):
             max_value = get_max(max_value, price[k] + stored_prices[j - k - 1])
@@ -50,30 +58,40 @@ def cut_bottom_up(price, n):
 # Helper function to run rod cutting algorithm
 def rod_cutting():
     print("")
+    # For lengths 1-10: Print max value
     for i in range(1, 11):
         print("Maximum value at length " + str(i) + ": ", cut_rod(rod_price, i))
     test_value = 8
     print("")
+    # Max value at length of test-value: 8
     print("Maximum bottom-up at length " + str(test_value) + ": ", cut_bottom_up(rod_price, test_value))
 
 
 # Greedy Algorithm implemented to solve scheduling tasks
+# start = start times, finish = finish times
 def get_greedy(start, finish):
+    # Length of acitivities - Start times must be same length as finish times
     n = len(start)
+    # a = Activities chosen
     a = []
+    # k = index
     k = 1
     for m in range(2, n):
+        # If start times after finish times add to activities selected
         if start[m] >= finish[k]:
             a.append(m)
             k = m
     return a
 
 
+# Runs program
 def main():
     rod_cutting()
+    # gets activities selected using a greedy algorithm
     activities = get_greedy(start_times, finish_times)
     output = "The greedy scheduling algorithm chooses activities: "
     print("")
+    # Format's output
     for activity in activities:
         output = output + str(activity) + " "
     print(output)
